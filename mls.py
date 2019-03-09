2b92e62ef9c8c06c55ca08d804a143f6eb8272a2
Cody Sharff <crsharff@gmail.com>
2019-03-09 14:03:32 -0800

    Add parsing of teams and the team details from the MLS tables - continue to build out the MLS API

--- mls.py
+++ mls.py
@@ -2,49 +2,83 @@ from bs4 import BeautifulSoup
 import requests
 
 MLS_STANDINGS_URL = "https://www.mlssoccer.com/standings"
-MLS_SCHEDULE_URL = "https://www.soundersfc.com/schedule"
+SOUNDERS_FC_SCHEDULE_URL = "https://www.soundersfc.com/schedule"
 NUMBER_OF_CONFERENCES = 2
-
-eastern_conference_teams = []
-western_conference_teams = []
+EASTERN_CONFERENCE = "Eastern"
+WESTERN_CONFERENCE = "Western"
+EASTERN_CONFERENCE_INDEX = 0
+WESTERN_CONFERENCE_INDEX = 1
 
 def getTeams():
+    return getEasternConferenceTeams() + getWesternConferenceTeams()
 
 def getEasternConferenceTeams():
-
-def getWesternConferenceTeams():
-
-def getStandings():
-    page = requests.get("https://www.mlssoccer.com/standings")
+    page = requests.get(MLS_STANDINGS_URL)
     soup = BeautifulSoup(page.content, 'html.parser')
     conference_tables = soup.find_all(class_="standings_table")
-    if conference_tables != NUMBER_OF_CONFERENCES:
-        return Exception
+    if len(conference_tables) != NUMBER_OF_CONFERENCES:
+       return Exception
 
-    eastern_conference = conference_tables[0]
-    western_conference = conference_tables[1]
-
-    for team in eastern_conference.select("tbody tr")[1:]: #Skip first element of standings table since it is a header
-        eastern_conference_teams.append(Team())
+    eastern_conference_table = conference_tables[EASTERN_CONFERENCE_INDEX]
+    eastern_conference_teams = parseTable(EASTERN_CONFERENCE, eastern_conference_table)
+    return eastern_conference_teams
 
 def getEasternConferenceStandings():
+    eastern_conference_teams = getEasternConferenceTeams()
+    return sorted(eastern_conference_teams, key=lambda team: team.rank)
+
+def getWesternConferenceTeams():
+    page = requests.get(MLS_STANDINGS_URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    conference_tables = soup.find_all(class_="standings_table")
+    if len(conference_tables) != NUMBER_OF_CONFERENCES:
+       return Exception
+
+    western_conference_table = conference_tables[WESTERN_CONFERENCE_INDEX]
+    western_conference_teams = parseTable(WESTERN_CONFERENCE, western_conference_table)
+    return western_conference_teams
 
 def getWesternConferenceStandings():
+    western_conference_teams = getWesternConferenceTeams()
+    return sorted(western_conference_teams, key=lambda team: team.rank)
 
 def getNumberOfTeamsInEasternConference():
-    return len(getEasternConferenceTeams)
+    return len(getEasternConferenceTeams())
 
-def getNumberofTeamsInWesternConference():
-    return len(getWesternConferenceTeams)
+def getNumberOfTeamsInWesternConference():
+    return len(getWesternConferenceTeams())
 
 def getSchedule(team):
+    return
 
+def parseTable(conference, conference_table):
+    teams = []
+    for team in conference_table.select("tbody tr")[1:]: #Skip first element of standings table since it is a header
+        club_name = team.find("td", {"data-title": "Club"}).select('a .hide-on-mobile-inline')[0].getText()
+        rank = int(team.find("td", {"data-title": "Rank"}).getText())
+        points = int(team.find("td", {"data-title": "Points"}).getText())
+        games_played = int(team.find("td", {"data-title": "Games Played"}).getText())
+        points_per_game = float(team.find("td", {"data-title": "Points Per Game"}).getText())
+        goals_for = int(team.find("td", {"data-title": "Goals For"}).getText())
+        goals_difference = int(team.find("td", {"data-title": "Goal Difference"}).getText())
+        wins = int(team.find("td", {"data-title": "Wins"}).getText())
+        losses = int(team.find("td", {"data-title": "Losses"}).getText())
+        ties = int(team.find("td", {"data-title": "Ties"}).getText())
+
+        details = TeamDetails(points, games_played, goals_for, goals_difference, wins, losses, ties)
+        team = Team(club_name, conference, rank, details)
+        teams.append(team)
+    return teams
 
 class Team:
-    def __init__(self, name, conference, details):
+    def __init__(self, name, conference, rank, details):
         self.name = name
         self.conference = conference
+        self.rank = rank
         self.details = details
+
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
 
 class TeamDetails:
     def __init__(self, points, games_played, goals_for, goals_difference, wins, losses, ties):
@@ -55,3 +89,6 @@ class TeamDetails:
         self.wins = wins
         self.losses = losses
         self.ties = ties
+
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
