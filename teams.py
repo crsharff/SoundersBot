0e339aa1230936afac99f220095b686678ca70a2
Cody Sharff <csharff@xevo.com>
2019-03-10 15:47:14 -0700

    Create a enum class that contains all the MLS teams together with their names and schedule URL.

--- teams.py
+++ teams.py
@@ -1,25 +1,46 @@
-class Team:
-    COLUMBUS_CREW_SC = "Columbus Crew SC"
-    TORONTO_FC = "Toronto FC"
-    DC_UNITED = "D.C. United"
-    MONTREAL_IMPACT = "Montreal Impact"
-    ORLANDO_CITY_SC = "Orlando City SC"
-    NEW_YORK_CITY_FC = "New York City FC"
-    NEW_YORK_RED_BULLS = "New York Red Bulls"
-    CHICAGO_FIRE = "Chicago Fire"
-    NEW_ENGLAND_REVOLUTION = "New England Revolution"
-    PHILADELPHIA_UNION = "Philadelphia Union"
-    ATLANTA_UNITED_FC = "Atlanta United FC"
-    FC_CINCINNATI = "FC Cincinnati"
-    SEATTLE_SOUNDERS_FC = "Seattle Sounders FC"
-    MINNESOTA_UNITED_FC = "Minnesota United FC"
-    FC_DALLAS = "FC Dallas"
-    HOUSTON_DYNAMO = "Houston Dynamo"
-    REAL_SALT_LAKE = "Real Salt Lake"
-    LOS_ANGELES_FOOTBALL_CLUB = "Los Angeles Football Club"
-    LA_GALAXY = "LA Galaxy"
-    PORTLAND_TIMBERS = "Portland Timbers"
-    COLORADO_RAPIDS = "Colorado Rapids"
-    SPORTING_KANSAS_CITY = "Sporting Kansas City"
-    VANCOUVER_WHITECAPS_FC = "Vancouver Whitecaps FC"
-    SAN_JOSE_EARTHQUAKES = "San Jose Earthquakes"
+from enum import Enum
+from collections import namedtuple
+
+Team = namedtuple('Team', ['name', 'scheduleUrl'])
+
+class Teams(Enum):
+
+    @property
+    def name(self):
+        return self.value.name
+
+    @property
+    def scheduleUrl(self):
+        return self.value.scheduleUrl
+
+    @staticmethod
+    def from_str(team_str):
+        for team in Teams:
+         if team_str.lower() == team.name.lower():
+            return team
+        print("Could not find team {}".format(team_str))
+
+    COLUMBUS_CREW_SC = Team("Columbus Crew SC", "https://www.columbuscrewsc.com/schedule")
+    TORONTO_FC = Team("Toronto FC", "https://www.torontofc.ca/schedule")
+    DC_UNITED = Team("D.C. United", "https://www.dcunited.com/schedule")
+    MONTREAL_IMPACT = Team("Montreal Impact", "https://www.impactmontreal.com/en/schedule")
+    ORLANDO_CITY_SC = Team("Orlando City SC", "https://www.orlandocitysc.com/schedule")
+    NEW_YORK_CITY_FC = Team("New York City FC", "https://www.nycfc.com/schedule")
+    NEW_YORK_RED_BULLS = Team("New York Red Bulls", "https://www.newyorkredbulls.com/schedule")
+    CHICAGO_FIRE = Team("Chicago Fire", "https://www.chicago-fire.com/schedule")
+    NEW_ENGLAND_REVOLUTION = Team("New England Revolution", "https://www.revolutionsoccer.net/schedule")
+    PHILADELPHIA_UNION = Team("Philadelphia Union", "https://www.philadelphiaunion.com/schedule")
+    ATLANTA_UNITED_FC = Team("Atlanta United FC", "https://www.atlutd.com/schedule")
+    FC_CINCINNATI = Team("FC Cincinnati", "https://www.fccincinnati.com/schedule")
+    SEATTLE_SOUNDERS_FC = Team("Seattle Sounders FC", "https://www.soundersfc.com/schedule")
+    MINNESOTA_UNITED_FC = Team("Minnesota United FC", "https://www.mnufc.com/schedule")
+    FC_DALLAS = Team("FC Dallas", "https://www.fcdallas.com/schedule")
+    HOUSTON_DYNAMO = Team("Houston Dynamo", "https://www.houstondynamo.com/schedule")
+    REAL_SALT_LAKE = Team("Real Salt Lake", "https://www.realsaltlake.com/schedule")
+    LOS_ANGELES_FOOTBALL_CLUB = Team("Los Angeles Football Club", "https://www.lafc.com/schedule")
+    LA_GALAXY = Team("LA Galaxy", "https://www.lagalaxy.com/schedule")
+    PORTLAND_TIMBERS = Team("Portland Timbers", "https://www.timbers.com/schedule")
+    COLORADO_RAPIDS = Team("Colorado Rapids", "https://www.coloradorapids.com/schedule")
+    SPORTING_KANSAS_CITY = Team("Sporting Kansas City", "https://www.sportingkc.com/schedule")
+    VANCOUVER_WHITECAPS_FC = Team("Vancouver Whitecaps FC", "https://www.whitecapsfc.com/schedule")
+    SAN_JOSE_EARTHQUAKES = Team("San Jose Earthquakes", "https://www.sjearthquakes.com/schedule")
